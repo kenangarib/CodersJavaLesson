2df856ed5da2b5e39deed6f72832cc2d88cf3ac1
Predator2000 <kenan.garibzadeh@gmail.com>
2022-10-09 11:16:07 +0400

    a ve b

--- src/task5/Task1.java
+++ src/task5/Task1.java
@@ -2,15 +2,24 @@ package task5;
 
 public class Task1 {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 8;
-
-        b = a + b - b;
-        a = a + b - 7 + a;
 
 
+        while(true){
+            int a = 5;
+            int b = 8;
+            a = (a + b) - a;
+            System.out.println("a = " + a);
+            break;
+        }
+        while (true){
+            int a = 5;
+            int b = 8;
+            b = (a + b) - b;
+            System.out.println("b = " + b);
+            break;
+        }
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+
+
     }
 }
