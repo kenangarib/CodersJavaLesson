7fe6007412b3392a178b9d543106b9614a794a67
Predator2000 <kenan.garibzadeh@gmail.com>
2022-12-06 19:14:47 +0400

    qwdqwdqwdqwd

--- src/az.coder.task1/GSMarena/PhoneFunctions/DateTime.java
+++ src/az.coder.task1/GSMarena/PhoneFunctions/DateTime.java
@@ -5,19 +5,23 @@ import java.time.format.DateTimeFormatter;
 
 public class DateTime {
     String strDate;
+    String localDateString;
     DateTimeFormatter dt;
     LocalDate localDate;
+    String strformat;
+
 
 
     public DateTime(String strDate) {
         this.strDate = strDate;
         this.dt = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         this.localDate = LocalDate.parse(strDate, this.dt);
+        this.strformat = localDate.format(dt);
 
     }
 
     @Override
     public String toString() {
-        return "\nRelease Time: " + localDate + "";
+        return "\nRelease Time: " + strformat + "";
     }
 }
--- src/az.coder.task1/GSMarena/PhoneFunctions/Processors/BionicA.java
+++ src/az.coder.task1/GSMarena/PhoneFunctions/Processors/BionicA.java
@@ -1,6 +1,6 @@
 package az.coder.task1.GSMarena.PhoneFunctions.Processors;
 
-public class BionicA extends Processor {
+public class BionicA <T> extends Processor {
     public BionicA(T ghz, T mm, T cores) {
         super(ghz, mm, cores);
     }
